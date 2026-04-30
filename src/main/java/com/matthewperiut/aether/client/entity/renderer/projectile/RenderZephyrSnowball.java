e3067b9521a6117449802abc897f0625c5ecefb7
Matigame <74415916+Matigamer64@users.noreply.github.com>
2026-04-30 12:08:28 +0200

    Fix Zephyr snowball invalid texture

--- src/main/java/com/matthewperiut/aether/client/entity/renderer/projectile/RenderZephyrSnowball.java
+++ src/main/java/com/matthewperiut/aether/client/entity/renderer/projectile/RenderZephyrSnowball.java
@@ -1,9 +1,15 @@
 package com.matthewperiut.aether.client.entity.renderer.projectile;
 
+import com.matthewperiut.aether.entity.projectile.EntityNotchWave;
 import com.matthewperiut.aether.entity.projectile.EntityZephyrSnowball;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
+import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
+import net.modificationstation.stationapi.api.client.texture.atlas.ExpandableAtlas;
+import net.modificationstation.stationapi.api.util.Identifier;
 import org.lwjgl.opengl.GL11;
 
 public class RenderZephyrSnowball extends EntityRenderer {
@@ -14,26 +20,23 @@ public class RenderZephyrSnowball extends EntityRenderer {
         GL11.glPushMatrix();
         GL11.glTranslatef((float) d, (float) d1, (float) d2);
         GL11.glEnable(32826);
-        float f2 = 2.0F;
-        GL11.glScalef(f2 / 1.0F, f2 / 1.0F, f2 / 1.0F);
-        int snowballIndex = 14;
-        this.bindTexture("gui/items.png");
+        float size = 2.0F;
+        GL11.glScalef(size, size, size);
+        Atlas.Sprite snowballSprite = Atlases.getGuiItems().getTexture(Identifier.of("minecraft:item/snowball"));
+        this.bindTexture("/gui/items.png");
         Tessellator tessellator = Tessellator.INSTANCE;
-        float f3 = (float) (snowballIndex % 16 * 16) / 256.0F;
-        float f4 = (float) (snowballIndex % 16 * 16 + 16) / 256.0F;
-        float f5 = (float) (snowballIndex / 16 * 16 + 0) / 256.0F;
-        float f6 = (float) (snowballIndex / 16 * 16 + 16) / 256.0F;
-        float f7 = 1.0F;
-        float f8 = 0.5F;
-        float f9 = 0.25F;
+        double minU = snowballSprite.getStartU();
+        double maxU = snowballSprite.getEndU();
+        double minV = snowballSprite.getStartV();
+        double maxV = snowballSprite.getEndV();
         GL11.glRotatef(180.0F - this.dispatcher.yaw, 0.0F, 1.0F, 0.0F);
         GL11.glRotatef(-this.dispatcher.pitch, 1.0F, 0.0F, 0.0F);
         tessellator.startQuads();
         tessellator.normal(0.0F, 1.0F, 0.0F);
-        tessellator.vertex((double) (0.0F - f8), (double) (0.0F - f9), 0.0, (double) f3, (double) f6);
-        tessellator.vertex((double) (f7 - f8), (double) (0.0F - f9), 0.0, (double) f4, (double) f6);
-        tessellator.vertex((double) (f7 - f8), (double) (1.0F - f9), 0.0, (double) f4, (double) f5);
-        tessellator.vertex((double) (0.0F - f8), (double) (1.0F - f9), 0.0, (double) f3, (double) f5);
+        tessellator.vertex(-0.5F, -0.25F, 0.0, minU, maxV);
+        tessellator.vertex(0.5F, -0.25F, 0.0, maxU, maxV);
+        tessellator.vertex(0.5F,  0.75F, 0.0, maxU, minV);
+        tessellator.vertex(-0.5F, 0.75F, 0.0, minU, minV);
         tessellator.draw();
         GL11.glDisable(32826);
         GL11.glPopMatrix();
